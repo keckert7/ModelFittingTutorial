5c0e59aa169849135905d1f7ef28c71193e419ea
keckert7 <keckert@physics.unc.edu>
2015-06-20 16:26:14 -0400

    updated from suggestions from Ashley and Sheila

--- paramfit1.py
+++ paramfit1.py
@@ -11,7 +11,7 @@ import pylab
 pylab.ion()
 
 
-#Generating fake data set to start with:
+# Generating fake data set to start with:
 alphatrue=2. # slope
 betatrue=5.  # intercept
 errs=2.5 # sigma (amplitude of errors)
@@ -19,55 +19,60 @@ errs=2.5 # sigma (amplitude of errors)
 narr=50. # number of data points
 xvals = np.arange(narr) + 1. # xvals range from 1-51
 yvals = alphatrue*xvals + betatrue+ npr.normal(0,errs,narr) # yvals 
-# what does npr.normal do?
+# What does npr.normal do?
 
-# plot fake data
+# Plot fake data
 plt.figure(1) 
 plt.clf()
 plt.plot(xvals,yvals,'b*',markersize=10)
+plt.xlabel("x-values")
+plt.ylabel("y-values")
 
-# determine slope & y-intercept using analytic solution from derivation sheet
+# Determine slope & y-intercept using analytic solution from derivation sheet
 
 #alphaest=(np.mean(xvals)*np.mean(yvals)-np.mean(xvals*yvals)) / \
-#   (np.mean(xvals)**2 -np.mean(xvals**2)) #  from derivation sheet
+#   (np.mean(xvals)**2 -np.mean(xvals**2)) #  from derivation
+#betaest= ? # calculate estimate of y-intercept from derivation
 
-#betaest= ? # calculate estimate of y-intercept 
 
+# What are MLE values of slope and y-intercept?
 
-# what are MLE values of slope and y-intercept?
+#print("analytical MLE slope = %f" %alphaest)
+#print("analytical MLE y-intercept = %f" %?)
 
-#print("slope = %f" %alphaest)
-#print("y-intercept = %f" %?)
-
-# overplot the best fit solution
+# Overplot the best fit solution
 #yfitvals=xvals*alphaest+betaest
 #plt.plot(xvals,yfitvals,'r')
 
-# compute analytic uncertainties on slope and y-intercept from derivation sheet
+# What have we assumed about the uncertainties on our data?
+
+# Compute analytic uncertainties on slope and y-intercept 
+
 #alphaunc=?
 #betaunc=?
 
-#print("uncertainty on alpha is %0.7f" % (alphaunc))
-#print("uncertainty on beta is %0.7f" % (betaunc))
+#print("analytical MLE uncertainty on alpha is %0.7f" % (alphaunc))
+#print("analytical MLE uncertainty on beta is %0.7f" % (betaunc))
 
 #print("fractional uncertainty on alpha is %0.7f" % (alphaunc/alphaest))
-#print("fractional uncertainty on beta is %0.7f" % (betaunc/betaest))
+#print("fractional uncertainty on beta is %0.7f" % (?))
 
 
-# solution using python solver np.polyfit
+# Solution using python solver np.polyfit
 # third parameter is order of fit, 1 for linear
 #pfit=np.polyfit(xvals,yvals,1) # returns highest power first 
 
-#print("slope = %f" %pfit[0])
-#print("y-intercept = %f" %?)
+#print("               ") # put in some whitespace to make easier to read
+#print("np.polyfit MLE slope = %f" %pfit[0])
+#print("np.polyfit MLE y-intercept = %f" %?)
 
 # Do you get the same result as in analytical case?
 
 # Note that not all problems have analytical solutions like linear regression
 
-# can also obtain errors from the diagonal terms of the covariance
-# matrix, which is the inverse of the Hessian matrix given on derivation sheet
-# also computed in np.polyfit...
+# Can also obtain errors from the diagonal terms of the covariance
+# matrix, which is the inverse of the Hessian matrix (from your pre-reading) and
+# can be computed in np.polyfit by setting cov='True'
 
 #pfit,covp=np.polyfit(xvals,yvals,1,cov='True') # returns highest power first
 # setting cov='True' returns the covariance matrix
@@ -75,8 +80,10 @@ plt.plot(xvals,yvals,'b*',markersize=10)
 #print("slope is %0.7f +- %0.7f" % (pfit[0], np.sqrt(covp[0,0])))
 #print("intercept is %0.7f +- %0.7f" % (?, ?)
 
-# are those errors the same as in analytical solution?
-# what happens if you increase the number of points?
-# what happens if you decrease the number of points?
+# Are those errors the same as in analytical solution?
+# What happens to the uncertainties if you increase/decrease the number of points used in the fit (try N=100, N=10) ?
+# What happens to the percentage difference between the analytical and numerical methods for computing the uncertanties if you increase/decrease the number of points (try N=100, N=10)?
+
+
 
 
--- paramfit2.py
+++ paramfit2.py
@@ -26,46 +26,55 @@ plt.plot(xvals,yvals,'b*',markersize=10)
 # Bayesian numerical solution finding the full
 # posterior probability distribution of a grid of models
 
-# setup the grids
+# Setup the grids
 gridsize1=1000
 gridsize2=100
 alphaposs=np.arange(gridsize1) / 100. # what values are we considering?
 betaposs=np.arange(gridsize2) / 10.  # and here?
 
-#print("min slope is %f and max slope is %f" % (np.min(alphaposs), ?)))
+#print("min slope is %f and max slope is %f" % (np.min(alphaposs), ?))
 #print("min y-int is %f and max y-int is %f" % (?, np.max(betaposs)))
 
-# what are our (implicit) priors?
+# What are our implicit priors?
 
-# but is that true? check that priors are actually equally spaced: 
-# plot lines with the different values of y-int or slope for a line from (0,1)
-xx=np.arange(0,1,0.1)  # set up array of dummy values
+
+# But what do these priors really mean? Check to see that our priors are 
+# actually equally spaced by plotting lines with the different values of y-int and slope for a line with x values ranging from (0,1)
+#xx=np.arange(0,1,0.1)  # set up array of dummy values
 
 # test y-intercept spacing
-plt.figure(2) 
-plt.clf()
-for i in range(len(betaposs)):       # loop over all y-int values
-    plt.plot(xx,xx+betaposs[i],'b-') # plot lines with different y-int values
+#plt.figure(2) 
+#plt.clf()
+#plt.subplot(121)
+#for i in range(len(betaposs)):       # loop over all y-int values
+#    plt.plot(xx,xx+betaposs[i],'b-') # plot lines with different y-int values
 
-plt.xlim(0,1)
-plt.ylim(0,1)
-plt.title("test y-int prior")
+#plt.xlim(0,1)
+#plt.ylim(0,1)
+#plt.xlabel("x values")
+#plt.ylabel("y values for several values of y-int (y=x+beta)")
+#plt.title("test y-int prior")
 # yes - evenly spaced uniform input distribution
 
+
 # test slope
-plt.figure(3)
-plt.clf()
-#for i in range(?):       # loop over all slope values
-#    plt.plot(?)          # plot lines with different slope values
+#plt.subplot(122)
+#for i in range(len(?)):       # loop over all slope values
+#    plt.plot(?,?)          # plot lines with different slope values
 
-#plt.xlim(?) # may need to zoom in
-#plt.ylim(?)
-plt.title("test slope prior")
+#plt.xlim(0,1) 
+#plt.ylim(0,0.2) # will need to zoom in
+#plt.xlabel("x values")
+#plt.ylabel("y values for several values of slope (y=alpha*x)")
+#plt.title("test slope prior")
 
-# does the flat prior in slope evenly cover parameter space? are flat or steep slopes covered more finely?
 
-# To use a flat grid set in slope & y-intercept, we must assume a prior that compensates for the unequal spacing in the slope direction
-# read through http://jakevdp.github.io/blog/2014/06/14/frequentism-and-bayesianism-4-bayesian-in-python/ for more details on obtaining this prior
+
+# A flat prior in slope amounts to an non-flat prior on the angle or tan(y/x), weighting our fit more heavily to steeper values of slope
+
+# We can them determine a prior that compensates for this unequal spacing in angle
+# Read through http://jakevdp.github.io/blog/2014/06/14/frequentism-and-bayesianism-4-bayesian-in-python/ for more details on obtaining this prior
+# Note that they have reversed the notation for the slope and y-intercept from our convention: prior is written as (1+slope**2)**(-3./2.)
 
 # remember the Bayesian likelihood is 
 #P(M|D)=P(D|M)*P(M)/P(D)
@@ -76,55 +85,66 @@ plt.title("test slope prior")
 #bayesianlike=exp(-1*chisq/2)*prior
 #lnbayesianlike=-1*chisq/2 + ln(prior)
 
-# compute likelihoods for all possible models in original setup
-lnlikes=np.zeros((gridsize1,gridsize2)) # setup an array to contain those values
+# compute likelihoods for all possible models with two different priors
+#lnlikes_flat=np.zeros((gridsize1,gridsize2)) # setup an array to contain those v#alues
+#lnlikes_comp=np.zeros((gridsize1,gridsize2)) # setup an array to contain those values
 
-for i in xrange(gridsize1):  # loop over all possible values of alpha
-    for j in xrange (gridsize2): # loop over all possible values of beta
-        modelvals = alphaposs[i]*xvals+betaposs[j] # compute yfit for given model
-        resids = (yvals - modelvals) # compute residuals for given grid model
-        chisq = np.sum(resids**2 / errs**2) # compute chisq likelihood
-        priorval=1.    # uniform prior
-#        priorval=?.   # prior to compensate for unequal spacing of slope
-         #lnlikes[i,j] = ? + ? 
+#for i in xrange(gridsize1):  # loop over all possible values of alpha
+#    for j in xrange (gridsize2): # loop over all possible values of beta
+#        modelvals = alphaposs[i]*xvals+betaposs[j] # compute yfit for given model
+#        resids = (yvals - modelvals) # compute residuals for given grid model
+#        chisq = np.sum(resids**2 / errs**2) # compute chisq likelihood
+#        priorval_flat=1.    # uniform prior
+#        priorval_comp=?   # prior to compensate for unequal spacing of slope
+#        lnlikes_flat[i,j] = (-1./2)*chisq + np.log(priorval_flat)      
+#        lnlikes_comp[i,j] = ? + ?
 
 
 # now we have a full array of likelihoods computed for each possible model
 # what if we want to know the probability distribution for the slope?
-# we can find out by marginalizing over the intercept
-# this means integrating over likelihoods so we have to take exp(lnlikes)
-likes=np.exp(lnlikes)
-marginalizedlikes_slope = np.sum(likes,axis=1) / np.sum(likes)
+# we can find out by "marginalizing" over the intercept or integrating over likelihoods 
+# First, we take exp(lnlikes)
+#likes_flat=np.exp(lnlikes_flat)
+#likes_comp=np.exp(lnlikes_comp)
+
+#Second, we sum over the y-intercept parameter and normalize
+#marginalizedlikes_flat_slope = np.sum(likes_flat,axis=1) / np.sum(likes_flat)
+#marginalizedlikes_comp_slope = np.sum(likes_comp,axis=1) / np.sum(likes_comp)
+
 # why do we sum over axis 1 in the numerator, but
 # the whole array in the denominator?
 
 # plot the likelihood distribution of slope values
-plt.figure(4) # plot of posterior prob. distribution for slope
-plt.clf()
-#plt.plot(alphaposs,marginalizedlikes_slope,'g.',markersize=10)
-plt.xlabel("alpha")
-plt.ylabel("posterior probability distribution of alpha")
+#plt.figure(3) # plot of posterior prob. distribution for slope
+#plt.clf()
+#plt.plot(alphaposs,marginalizedlikes_flat_slope,'g.',markersize=10)
+#plt.plot(?,?,'r.',markersize=10)
+#plt.xlabel("alpha")
+#plt.ylabel("marginalized likelihood distribution of slope")
 
 # zoom in on the region of significant probability
 # and estimate the error from the graph
-#plt.xlim(1.5,2.5)
+
 
 # now marginalize over the slope to see the posterior distribution in y-intercept
-#marginalizedlikes_yint = ?
-plt.figure(5)
-plt.clf()
-#plt.plot(?)
-#plt.xlabel(?)
-#plt.ylabel(?)
+#marginalizedlikes_flat_yint = ?
+#marginalizedlikes_comp_yint = ?
+
+#plt.figure(4)
+#plt.clf()
+#plt.plot(betaposs,marginalizedlikes_flat_yint,'g',markersize='10.')
+#plt.plot(betaposs,marginalizedlikes_comp_yint,'r',markersize='10.')
+#plt.xlabel("beta")
+#plt.ylabel("marginalized likelihood distribution of y-intercept")
 
 
 
-# how do the values of the slope & y-intercept compare with the MLE values from paramfit1.py?
+# Estimate from the plots the best values Bayesian slope and y-intercept. How do the values of the slope & y-intercept compare with the MLE values from paramfit1.py?
 
-# how does the error on the slope & y-intercept compare with the value from the covariance matrix from paramfit1.py?
+# How does the error on the slope & y-intercept compare with the value from the covariance matrix from paramfit1.py?
 
-# what happens to the values and uncertainties if you change the number of points in your data?
+# What happens to the values and uncertainties if you change the number of points in your data (try N=100, N=10)? 
 
-# what happens if you change the grid spacing?
+# what happens if you change the grid spacing (try slope ranges from 1-10 in steps of 0.1, y-int ranges from 1-10 in steps of 1)? 
 
 
